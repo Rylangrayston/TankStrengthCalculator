8efc724d2e219d36984a2ad726cf60183efdebee
rylangrayston <rylangrayston@gmail.com>
2015-01-11 17:32:54 -0500

    This is townlys work! but im commiting for him Thanks Townley!! .. cleaned up code, checked math looks good, changed units to more commonly used ones, made the text formating of the output nicer to read

--- tank-calc.py
+++ tank-calc.py
@@ -1,5 +1,6 @@
+import math
 
-#This scrip takes 4 paramaters describing an open top cylindrical tank, it dose a bit of math and tell you how much 
+# This script takes 4 paramaters describing an open top cylindrical tank, it dose a bit of math and tell you how much 
 # force would be exurted on the very bottom bolt or rivet that is holding seam in the tank together, the shearing strength
 # of the fastener would need to be about 3 times the shear force exurted on it to be cosiderd safe as a peachy printer tank 
 # as it will have repeated loadings put on it. 
@@ -8,54 +9,40 @@
 def centimetersFromInches(inches):
     return(inches*2.54)
 
-
-
 # change these variables:
 
 centimetersPerFastener = 1.3 # use cm This is the distance between the center hole of each fastener in your seam of your tank in the hight direction. 
-
-cercumference = centimetersFromInches(90.0) # use cm, the final cercumference of your tank ( after over laps of seams)
-
+circumference = centimetersFromInches(90.0) # use cm, the final cercumference of your tank ( after over laps of seams)
 height = centimetersFromInches(96.0) # use cm, this is the Highes levle you will fill your tank to 
-
 densityOfSatruatedSaltWater = 1.202 # use g/ml  or g/cubic-cm , this is for NaCl 
-
 dollarsPerGramOfSalt =  5.0 / (20.0 * 1000.0) # use cad per gram .... ie  5 dolars / 20k grams 
 
-
-import math
-
-
-
-
-
-def diameterFromCercumference(cercumference):
-    return(cercumference/math.pi)
+def diameterFromCircumference(circumference):
+    return(circumference / math.pi)
 
 def radiusFromDiameter(diameter):
-    return(diameter/2)
+    return(diameter / 2.0)
 
-def ariaFromRadius(radius):
-    return(math.pi * radius *radius)
+def areaFromRadius(radius):
+    return(math.pi * radius * radius)
 
 def volumeFromRadiusAndHeight(radius, height):
     return(math.pi * radius * radius * height)
 
-def weightFromVolumeAndDensity(volume,density):  #needs units
+def weightFromVolumeAndDensity(volume, density):  #needs units
     return(volume * density)
 
-def hoopAriaPerFastenerFromDiameterAndCentimetersPerFastener(diameter,centimetersPerFastener):
+def hoopAreaPerFastenerFromDiameterAndCentimetersPerFastener(diameter, centimetersPerFastener):
     return(diameter * centimetersPerFastener)
 
-def volumeAboveHoopAriaPerFastenerFromHoopAriaPerFastenerAndHeight(hoopAriaPerFastener, height):
-    return(hoopAriaPerFastener * height)
-    
+def volumeAboveHoopAreaPerFastenerFromHoopAreaPerFastenerAndHeight(hoopAreaPerFastener, height):
+    return(hoopAreaPerFastener * height)
 
-def wieghtOnHoopAriaFromVolumeAboveHoopAriaPerFastenerAndDensity(volumeAboveHoopAriaPerFastener, density):
-    return(volumeAboveHoopAriaPerFastener* density)
+def wieghtOnHoopAreaFromVolumeAboveHoopAreaPerFastenerAndDensity(volumeAboveHoopAreaPerFastener, density):
+    return(volumeAboveHoopAreaPerFastener * density)
 
-def shearForceFromWeightOnHoopAria(weightOnHoopAria):
-    return(weightOnHoopAria/2)
+def shearForceFromWeightOnHoopArea(weightOnHoopArea):
+    return(weightOnHoopArea / 2.0)
 
 def poundsFromGrams(grams):
     return(grams*.00220462)
@@ -67,45 +54,35 @@ def costOfSalt(massOfSalt, dollarsPerGramOfSalt):
     return(massOfSalt * dollarsPerGramOfSalt)
 
 
-
-diameter = diameterFromCercumference(cercumference) # cm
-
+diameter = diameterFromCircumference(circumference) # cm
 radius = radiusFromDiameter(diameter) #cm
-
-hoopAriaPerFastener = hoopAriaPerFastenerFromDiameterAndCentimetersPerFastener(diameter,centimetersPerFastener) #cm
-
-aria = ariaFromRadius(radius) # cm squar
-
+hoopAreaPerFastener = hoopAreaPerFastenerFromDiameterAndCentimetersPerFastener(diameter,centimetersPerFastener) #cm
+area = areaFromRadius(radius) # cm squar
 volume = volumeFromRadiusAndHeight(radius, height) # cm cubed == ml 
-
 weight = weightFromVolumeAndDensity(volume, densityOfSatruatedSaltWater) # 1ml of water is 1 gram of weight
-
-volumeAboveHoopAriaPerFastener = volumeAboveHoopAriaPerFastenerFromHoopAriaPerFastenerAndHeight(hoopAriaPerFastener, height) # cm qubed
-
-weightOnHoopAria = wieghtOnHoopAriaFromVolumeAboveHoopAriaPerFastenerAndDensity(volumeAboveHoopAriaPerFastener, densityOfSatruatedSaltWater)  # retruns in grams 
-
-shearForce = shearForceFromWeightOnHoopAria(weightOnHoopAria)  # return in grams
-
+volumeAboveHoopAreaPerFastener = volumeAboveHoopAreaPerFastenerFromHoopAreaPerFastenerAndHeight(hoopAreaPerFastener, height) # cm qubed
+weightOnHoopArea = wieghtOnHoopAreaFromVolumeAboveHoopAreaPerFastenerAndDensity(volumeAboveHoopAreaPerFastener, densityOfSatruatedSaltWater)  # retruns in grams 
+shearForce = shearForceFromWeightOnHoopArea(weightOnHoopArea)  # return in grams
 massOfSalt = massOfSalt(weight)
-
 costOfSalt = costOfSalt(massOfSalt, dollarsPerGramOfSalt)
 
-print cercumference, ' cercumference in cm'
-print height, ' height in cm'
-print densityOfSatruatedSaltWater, ' densityOfSatruatedSaltWater in g/ml or g/cubic-cm'
-print diameter, ' diameter in cm'
-print radius, ' radius in cm'
-print aria, ' aria in cm square'
-print volume, ' volume in ml or cubic cm'
-print weight, ' weight or mass in grams'
-print hoopAriaPerFastener, ' hoopAriaPerFastener in cm square'
-print volumeAboveHoopAriaPerFastener, ' volumeAboveHoopAriaPerFastener in cm qubed'
-print weightOnHoopAria, ' weightOnHoopAria in grams'
-print shearForce, ' shearForce in grams'
-print poundsFromGrams(shearForce), ' shear force in pounds'
-print shearForce/1000, ' shear force in kg'
-print massOfSalt, ' massOfSalt in grams'
-print costOfSalt, ' cost of total salt needed to saturate full tank in dollars'
+
+print ('%10.2f cm    circumference' % circumference)
+print ('%10.2f cm    height' % height)
+print ('%10.2f g/ml  density Of Satruated Salt Water' % densityOfSatruatedSaltWater)
+print ('%10.2f cm    diameter' % diameter)
+print ('%10.2f cm    radius' % radius)
+print ('%10.2f cm^2  area' % area)
+print ('%10.2f m^2   area' % (area / (100.0 * 100.0)))
+print ('%10.2f l     volume' % (volume / 1000.0))
+print ('%10.2f kg    weight' % (weight / 1000.0))
+print ('%10.2f cm^2  hoop Area Per Fastener' % hoopAreaPerFastener)
+print ('%10.2f cm^3  volume Above Hoop Area Per Fastener' % volumeAboveHoopAreaPerFastener)
+print ('%10.2f kg    weight On Hoop Area' % (weightOnHoopArea / 1000.0))
+print ('%10.2f kg    shear force' % (shearForce / 1000.0))
+print ('%10.2f lbs   shear force' % poundsFromGrams(shearForce))
+print ('%10.2f kg    mass Of Salt' % (massOfSalt / 1000.0))
+print ('%10.2f $     cost of total salt needed to saturate full tank in dollars' % costOfSalt)
 
 
 
@@ -122,5 +99,4 @@ print costOfSalt, ' cost of total salt needed to saturate full tank in dollars'
 #http://en.wikipedia.org/wiki/Brine
 
 # http://www.homedepot.ca/product/20-kg-crystal-plus-water-softener-salt/966409
-
 
